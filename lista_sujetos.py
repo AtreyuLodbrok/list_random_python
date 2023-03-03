2e02086a14b62547ef8c4bc5f1c70cac1ad8d8cd
AtreyuLodbrok <coopervan6@gmail.com>
2023-03-02 20:27:17 -0600

    Primera parte

---
+++ lista_sujetos.py
@@ -0,0 +1,50 @@
+import numpy as np
+import random
+from faker import Faker
+
+
+
+numeros_de_cuenta = []
+nombres_alumnos = []
+materias_reprobadas = []
+def crea_numeros_cuenta():
+
+    for i in range(400):
+
+        cuenta = ''.join([str(random.randrange(9))for j in (range(10))])
+        numeros_de_cuenta.append(cuenta)
+
+    print(numeros_de_cuenta)
+    print(len(numeros_de_cuenta))
+
+def crea_nombres():
+    fake=Faker()
+
+    for i in range(400):
+        nombre = fake.name()
+        nombres_alumnos.append(nombre)
+
+    print(nombres_alumnos)
+
+def adeudo_materias():
+   
+
+    for i in range(10):
+
+        x = random.randint(0,1)
+
+        if x == 1:
+            materias_reprobadas.append("yes")
+        if x == 0:
+            materias_reprobadas.append("no")
+    
+    print(materias_reprobadas)
+
+
+
+
+#crea_numeros_cuenta()
+#crea_nombres()
+adeudo_materias()
+
+
